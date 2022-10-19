2b4aec932df718ccc44e0f5a182ad26a36568715
Caio Lucas <ccaio.oliveira83@gmail.com>
2022-10-19 18:29:41 -0300

    adicionando projeto

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import Primeiro from './components/Primeiro';
+
+const App = () => {
+    return (
+        <View><Primeiro /></View>
+    )
+}
+
+export default App;
---
+++ src/components/Primeiro.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Primeiro = () => {
+    return (
+        <Text>Primeiro!</Text>
+    )
+}
+
+export default Primeiro;
